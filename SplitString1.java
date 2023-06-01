cec8b74d579ce745c5cd1598caa96dbdedc78871
Arvind mandre <118597882+aaru2599@users.noreply.github.com>
2023-06-01 20:44:53 +0530

    Update SplitString1.java

--- SplitString1.java
+++ SplitString1.java
@@ -4,7 +4,7 @@ public class SplitString1 {
 	public static void main(String[] args) {
 		
 	
-	String s1="hello,savan ,bhosdiwale";
+	String s1="hello,savan ,Arvind";
 	String []a=s1.split(",");
 	for(String y:a) {
 		System.out.println(y);
